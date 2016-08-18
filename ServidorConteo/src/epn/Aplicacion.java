31f2779790742da80836ee53bb589739fd70d8dc
sandysimbana <sindy_5593@hotmail.com>
2016-08-17 20:47:40 -0500

    Update Aplicacion.java

--- ServidorConteo/src/epn/Aplicacion.java
+++ ServidorConteo/src/epn/Aplicacion.java
@@ -4,6 +4,8 @@ package epn;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,24 +13,33 @@ import java.util.logging.Logger;
 public class Aplicacion {
 
 	public static void main(String[] args) {
-		
-		ServerSocket ss;
+	
+	
+        ServerSocket ss=null;
+        Socket socket=null;
         System.out.print("Inicializando servidor... ");
         try {
-            ss = new ServerSocket(5555);
+            ss = new ServerSocket(5555,5);
             System.out.println("\t[OK]");
             while (true) {
-                Socket socket;
+                
                 socket = ss.accept();
-                System.out.println("Nueva conexión entrante: "+socket);
-                ((Servidor) new Servidor(socket)).start();
+                System.out.println("Nueva conexión entrante: "+socket+"     Hora: "+new Date());
+               ((Servidor) new Servidor(socket)).start();
               
             }
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }finally{
+            try {
+                ss.close();
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger("Error al cerrar el servidor: " + ex.getMessage());
+            }
 		
 
 	}
+        }
 
 }
