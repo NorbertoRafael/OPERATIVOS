fa36660d91cdd12cf2e2630d8a82effe57fecaff
GonzaloDavid <davidgonzalomejia@hotmail.com>
2016-08-21 07:08:24 -0500

    Update MainCliente.java
    comentario

--- Cliente/src/com/epn/sistemas/MainCliente.java
+++ Cliente/src/com/epn/sistemas/MainCliente.java
@@ -5,11 +5,11 @@ public class MainCliente {
     
     public static void main(String[] args) {
         
-        InterfazCliente Icliente = new InterfazCliente(); 
-        Icliente.show();
+        InterfazCliente Icliente = new InterfazCliente();                       //instanciamos la clase interfaz
+        Icliente.show();                                                        //Mostramos la interfaz(Jpanel)
       }
     public static void IniciarHilos(String ip){
-        cliente = new ConetorCliente(ip); 
+        cliente = new ConetorCliente(ip);                                   //INTANCIAMOS un objeto de la clase cliente para utilizar el metodo strart o run
         cliente.start();
     }
     
